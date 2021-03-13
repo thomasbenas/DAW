0e993026df895b4d8c3fb251874ed8a3f8ffa000
Thomas Bénas <benas.thomas@gmail.com>
2021-03-13 20:35:49 +0100

    erreur syntax controller

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 define('ROOT', str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
 
 require_once(ROOT.'app/Model.php');
-require_once(ROOT.'app/Conroller.php');
+require_once(ROOT.'app/Controller.php');
