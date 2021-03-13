8d03750788ca4577b970c6e4bbb837d0d4c8a032
Thomas Bénas <benas.thomas@gmail.com>
2021-03-13 08:48:22 +0100

    ajout index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+
+require_once 'vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+
+$app->router->get('/', function () {
+    return 'Hello World';
+});
+
+$app->run();
