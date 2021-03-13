f1610f17fce99c09fc7f40ea11ef8eb48f9acf90
Thomas Bénas <benas.thomas@gmail.com>
2021-03-13 23:46:20 +0100

    ajout de la méthode loadModel

--- core/Controller.php
+++ core/Controller.php
@@ -2,7 +2,15 @@
 
 namespace app\core;
 
+use app\models;
+
 abstract class Controller
 {
+
+    public function loadModel(string $model) :void
+    {
+        $modelNameSpace = "\app\models\\". $model;
+        $this->$model = new $modelNameSpace();
+    }
 
 }
