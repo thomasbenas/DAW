cbb3c2514fa0c16e071956c9ff61c04aed06b60c
Thomas Bénas <benas.thomas@gmail.com>
2021-03-14 13:31:06 +0100

    [add] template vues

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -16,7 +16,11 @@ abstract class Controller
     public function render(string $folder, string $file, array $data = [])
     {
         extract($data, null);
+
+        ob_start();
         require_once(ROOT.'src/views/'.$folder.'/'.$file.'.php');
+        $content = ob_get_clean();
+        require_once(ROOT.'src/views/layout/base.php');
 
     }
 
