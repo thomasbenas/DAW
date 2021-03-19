fd3c80a6e407c55053d2211e3fa3e400ac925001
Thomas Bénas <benas.thomas@gmail.com>
2021-03-19 23:00:06 +0100

    [fix] css non accessible depuis un dossier

--- src/views/layout/base.php
+++ src/views/layout/base.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="../../../assets/css/style.css" rel="stylesheet">
+    <link href="//localhost/../../../assets/css/style.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body id="page-container">
