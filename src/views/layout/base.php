4658c31c7e7cfc37bb96f2a696c8af1822a2c5e8
Thomas Bénas <benas.thomas@gmail.com>
2021-03-21 10:19:00 +0100

    [update] structure gabarit de page

--- src/views/layout/base.php
+++ src/views/layout/base.php
@@ -1,20 +1,17 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="//localhost/../../../assets/css/style.css" rel="stylesheet">
-    <title>Document</title>
+    <?php require_once(ROOT.'src/views/layout/head.php') ?>
 </head>
 <body>
     <div class="wrapper">
-        <header class="page-header">This is the header</header>
+        <header class="page-header">
+            <?php require_once(ROOT.'src/views/layout/menu.php') ?>
+        </header>
         <article class="page-body">
             <?= /** @var string $content */ $content ?>
         </article>
-        <footer class="page-footer">Sticky footer</footer>
+        <?php require_once(ROOT.'src/views/layout/footer.php') ?>
     </div>
 </body>
 </html>
