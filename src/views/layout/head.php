b300b4cb04dddcbdc36c60d1c2ade6514fb41fa6
Thomas Bénas <benas.thomas@gmail.com>
2021-03-21 10:20:42 +0100

    [add] fonts roboto et quando

--- src/views/layout/head.php
+++ src/views/layout/head.php
@@ -2,4 +2,6 @@
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link href="//localhost/../../../assets/css/style.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Quando&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 <title>Document</title>
