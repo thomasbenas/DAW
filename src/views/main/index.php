60d10c7ea024cfe282f93d6905f451d2b0607a8e
Thomas Bénas <benas.thomas@gmail.com>
2021-03-22 15:35:13 +0100

    [fix] lien relatif pr le css et les svg

--- src/views/main/index.php
+++ src/views/main/index.php
@@ -28,7 +28,7 @@
                 <h3>Par des étudiants, pour des étudiants</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id temporibus consequuntur laborum a optio. Laborum expedita ratione fugit esse, consectetur incidunt. Maiores sit excepturi quae nisi ab sapiente aspernatur dignissimos!</p>
             </div>
-            <img class="list-img" src="//localhost/assets/images/online-courses.svg" alt="">
+            <img class="list-img" src="./assets/images/online-courses.svg" alt="">
         </div>
 
         <div class="list">
@@ -36,7 +36,7 @@
                 <h3>Pas de magie seulement du code !</h3>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum a mollitia repellendus neque delectus commodi provident porro! Consequatur, dolores laborum deserunt ducimus aliquid beatae ullam ipsum adipisci quam. Est, tempore!</p>
             </div>
-            <img class="list-img" src="//localhost/assets/images/code.svg" alt="">
+            <img class="list-img" src="./assets/images/code.svg" alt="">
         </div>
 
         <div class="list">
@@ -44,7 +44,7 @@
                 <h3>Sans compter une interface étudiée pour faciliter l'apprentissage</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia reiciendis facilis qui dolore quod illo saepe. Eum libero eveniet quibusdam vero, dolor facilis adipisci dignissimos, ex, nemo pariatur corporis eius.</p>
             </div>
-            <img class="list-img" src="//localhost/assets/images/book.svg" alt="">
+            <img class="list-img" src="./assets/images/book.svg" alt="">
         </div>
     </div>
     
