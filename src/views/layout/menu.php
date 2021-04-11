37e7873d3e78c92ff71741d51d7ea2adb5307d21
Thomas Bénas <benas.thomas@gmail.com>
2021-04-11 17:46:09 +0200

    [add] liens dans le menu

--- src/views/layout/menu.php
+++ src/views/layout/menu.php
@@ -1,9 +1,9 @@
 <div class="page-nav">
-    <div class="page-nav-title">ClosedClassroom</div>
+    <div class="page-nav-title"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>">ClosedClassroom</a></div>
     <div class="page-nav-block">
-        <div class="page-nav-item"><a href="#">QCM</a></div>
-        <div class="page-nav-item"><a href="#">Cours</a></div>
-        <div class="page-nav-item"><a href="#">Forum</a></div>
+        <div class="page-nav-item"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/qcm/">QCM</a></div>
+        <div class="page-nav-item"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/cours/">Cours</a></div>
+        <div class="page-nav-item"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/forum/">Forum</a></div>
         <?php if (true): //TODO déterminer si l'utilisateur est connecté ou non?>
             <div class="page-nav-item button"><a href="#">Rejoindre</a></div>
             <div class="page-nav-item button"><a href="#">Connexion</a></div>
