cd9ccfde53f3803b2e85664ae9a314a9b874915b
Thomas Bénas <benas.thomas@gmail.com>
2021-04-11 17:45:46 +0200

    [add] page erreur 404

---
+++ src/views/error/404.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="error">
+        <div class="error_img"></div>
+        <div class="error_text">
+            <span>Oups ! la page n'existe pas</span>
+        </div>
+    </div>
+</div>
