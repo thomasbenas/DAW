a96e769344c3e54ff1c1b3e5c7cd7b364fc92e1b
Thomas Bénas <benas.thomas@gmail.com>
2021-04-14 16:06:43 +0200

    [update] page de connexion fonctionnel

--- src/views/utilisateur/connexion.php
+++ src/views/utilisateur/connexion.php
@@ -6,12 +6,11 @@
     <div class="form">
         <?php if(!empty($_POST['userName']) && !empty($_POST['userPassword'])): ?>
             <?php require_once(ROOT.'src/views/utilisateur/connexionAction.php') ?>
-        <?php else: ?>
-            <div class="form-head">
-                <h3>Connexion</h3>
-                <img src="../assets/images/secure-login.svg" alt="">
-            </div>
-            <?php require_once(ROOT.'src/views/utilisateur/connexionForm.php') ?>
         <?php endif; ?>
+        <div class="form-head">
+            <h3>Connexion</h3>
+            <img src="../assets/images/secure-login.svg" alt="">
+        </div>
+        <?php require_once(ROOT.'src/views/utilisateur/connexionForm.php') ?>  
     </div>
 </div>
--- src/views/utilisateur/connexionAction.php
+++ src/views/utilisateur/connexionAction.php
@@ -1,3 +1,16 @@
 <?php 
+$utilisateur->userLogin(htmlspecialchars($_POST['userName']),htmlspecialchars( $_POST['userPassword'])); 
+if(isset($_GET['error'])):
+    $error = htmlspecialchars($_GET['error']);
+else:
+?>
 
-$utilisateur->test();
+<div id="toast"></div>
+
+<script>
+    window.onload = function() {
+        LoginLogoutMessage('<i class=\'fas fa-check\'></i> Connexion réussie !')
+    };
+</script>
+
+<?php endif; ?>
