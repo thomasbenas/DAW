605688d61ed358770a9b668e62e2522fc0eb8fc8
Thomas Bénas <benas.thomas@gmail.com>
2021-04-14 14:48:16 +0200

    [update] bouton profil

--- src/views/layout/menu.php
+++ src/views/layout/menu.php
@@ -8,7 +8,6 @@
             <div class="button-fill"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/utilisateur/rejoindre">REJOINDRE</a></div>
             <div class="button"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/utilisateur/connexion">CONNEXION</a></div>
         <?php else: ?>
-            <div class="page-nav-item button-img"><a href="#">Profil</a></div>
             <div class="button-fill"><a href="#">Profil de <?= $_SESSION['username'] ?></a></div>
             <div class="button"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/utilisateur/deconnexion">DECONNEXION</a></div>
         <?php endif ?>
