3325e461ea644f8ece6cc0d4cae265e2e0132a16
Thomas Bénas <benas.thomas@gmail.com>
2021-04-14 16:07:51 +0200

    [update] syntaxe fonction

--- src/views/utilisateur/rejoindreAction.php
+++ src/views/utilisateur/rejoindreAction.php
@@ -1,5 +1,6 @@
 <?php 
 $utilisateur->UserRegistration(htmlspecialchars($_POST['userName']),htmlspecialchars( $_POST['userPassword']), htmlspecialchars($_POST['userMail'])); 
+$utilisateur->userRegistration(htmlspecialchars($_POST['userName']),htmlspecialchars( $_POST['userPassword']), htmlspecialchars($_POST['userMail'])); 
 if(isset($_GET['error']))
     $error = htmlspecialchars($_GET['error']);
 else{
