14f04aa4ff05183e29c56a9a05b072d79188a7dd
Thomas Bénas <benas.thomas@gmail.com>
2021-04-14 14:38:01 +0200

    [fix] nom de la table

--- src/models/UserModel.php
+++ src/models/UserModel.php
@@ -13,7 +13,7 @@ class UserModel extends Model
     public function __construct()
     {
         parent::__construct();
-        $this->table = "test";
+        $this->table = "users";
         $this->connection();
     }
     
