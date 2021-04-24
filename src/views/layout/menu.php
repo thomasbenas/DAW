70c46a4e6124d75e84247d947041ad690cf12f06
Thomas Bénas <benas.thomas@gmail.com>
2021-04-24 14:39:56 +0200

    [add] bouton "panel admin"

--- src/views/layout/menu.php
+++ src/views/layout/menu.php
@@ -9,6 +9,9 @@
         <?php if (!isset($_SESSION['username'])): ?>
             <div class="button-fill"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/utilisateur/rejoindre">REJOINDRE</a></div>
             <div class="button"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/utilisateur/connexion">CONNEXION</a></div>
+        <?php elseif($_SESSION['admin']): ?>
+            <div class="button-fill"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/admin">PANEL ADMIN</a></div>
+            <div class="button"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/utilisateur/deconnexion">DECONNEXION</a></div>
         <?php else: ?>
             <div class="button-fill"><a href="#">Profil de <?= $_SESSION['username'] ?></a></div>
             <div class="button"><a href="//<?= HOST . '/' .FOLDER_ROOT ?>/utilisateur/deconnexion">DECONNEXION</a></div>
