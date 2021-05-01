8a6962406f92d39cdf7c2cc9d5b69c4212736466
ChantonyTY <74354624+ChantonyTY@users.noreply.github.com>
2021-05-01 16:44:24 +0200

    [UPDATE] VUE QUIZZ (CORRECTION NE MARCHE PAS)
    t'es chauve

--- src/views/quiz/voir.php
+++ src/views/quiz/voir.php
@@ -4,7 +4,7 @@
 ?>
 <div class="container">
     <h2>Quiz web</h2>
-    <form action="http://localhost/DAW/quiz" method="post" class="quiz">
+    <form action="http://localhost/DAW/quiz/voir/<?=$quizs['slug']?>" method="post" class="quiz">
         <?php foreach($quizz->question as $question): $numberQuestions++;?>
             <div class="blockQuestion">
                 <div class="question"><?='Question ' . $numberQuestions . " : " . $question->libelle?></div>
@@ -14,7 +14,32 @@
                 <?php endforeach?>
             </div>
         <?php endforeach ?>
-        <input class="button-fill" type="submit" name="submit" value="Terminer le quiz">
+        <input class="button-fill" type="submit" name="validerQuiz" value="Terminer le quiz">
     </form>
+    <?php 
+    if(isset($_POST['validerQuiz']))
+    {
+        $total = 0;
+        foreach ($_POST as $key => $value){
+            if (isset($key) and $key != "validerQuiz"){
+                    $total += $value;
+            }
+        }
+        if ($total >= 0){
+            echo "Votre score est de ".$total. ". Voici les bonnes réponses.";
+        }else{
+            echo "erreur";
+        }
+        echo '<script type="text/javascript">
+        var reponses = document.getElementsByClassName("reponse");
+        for (i = 0; i < reponses.length; i++) {
+            if(reponses[i].value == 1){
+                reponses[i].checked = true;
+            }
+          }
+          </script>';
+    } 
+    ?>
 </div>
+
 
